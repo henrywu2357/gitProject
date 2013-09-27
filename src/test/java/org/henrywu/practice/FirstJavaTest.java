2184751d8040d4e3690f99f65bd14fd43272bf1e
ewwuxin <ewwuxin@E7B499BAE43BBD.ericsson.se>
2013-09-27 15:46:17 +0800

    add ut

---
+++ src/test/java/org/henrywu/practice/FirstJavaTest.java
@@ -0,0 +1,16 @@
+package org.henrywu.practice;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class FirstJavaTest {
+	
+	@Test
+	public void case1(){
+		int a = 1;
+		int b = 1;
+		assertTrue(a==b);
+	}
+	
+
+}
